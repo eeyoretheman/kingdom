dc025c90d8f273baf531c78be0dc9707fa60aac3
Yordan Todorov <yordan.iliev.todorov@gmail.com>
2024-03-26 23:08:30 +0200

    chore: improve notifier and teller, fix types

--- Server/cmd/app/main.go
+++ Server/cmd/app/main.go
@@ -2,26 +2,45 @@ package main
 
 import (
 	"fmt"
-	"kingdom/internal/listeners"
+	listeners "kingdom/internal/listeners"
+	tellers "kingdom/internal/tellers"
 )
 
 // run listeners when a menu option is selected with the address and port
-func print_menu() {
+func printMenu() {
 	fmt.Println("1. Start listener")
 	fmt.Println("2. Exit")
 	fmt.Println("3. Manual connect to listener")
 	fmt.Print("Enter your choice: ")
 }
 
+// CLI + Backend all in 1 for now; separate later
 func main() {
-	print_menu()
+	printMenu()
+
+	var tellerSlice []tellers.Teller
+	var active tellers.Teller
+	callback := make(chan tellers.Teller)
+
 	var choice int
 	fmt.Scanln(&choice)
-	switch choice {
 
+	switch choice {
 	case 1:
-		listen := listeners.Listener{Addr: "127.0.0.1", Port: 1337}
-		listeners.Run(listen)
+		listener := listeners.Listener{Addr: "127.0.0.1", Port: 1337}
+		listeners.Run(listener, callback)
+
+		for {
+			select {
+			case t := <-callback:
+				fmt.Printf("Added %s\n", t.Name)
+				tellerSlice = append(tellerSlice, t)
+				active = t // for testing
+			case data := <-active.Output:
+				fmt.Printf("Input from '%s': %s", active.Name, data)
+				active.Input <- []byte("ls\n")
+			}
+		}
 	case 2:
 		fmt.Println("Exiting...")
 
